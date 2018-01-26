dc09ba8a52597c99a507c51cdf6ce31aa54ed496
Arjun Variar <accio.arjun@gmail.com>
2018-01-26 14:15:00 +0530

    Adding in bleu scores.

--- pix2code.py
+++ pix2code.py
@@ -1,55 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import torch
 from torch.utils.model_zoo import load_url
 from seq2seq.models import Img2Seq
 from seq2seq.tools.inference import CaptionGenerator
+from seq2seq.tools.bleu import get_bleu
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from scipy.misc import imresize
 import cv2
-
+from seq2seq.datasets.pix2codedataset import Pix2CodeDataset
+import progressbar
 
 def show_and_tell(filename):
     img = cv2.imread(filename)
+    img = cv2.resize(img, (256,256))
     plt.figure()
     plt.imshow(np.asarray(img))
     return img, caption_model.describe(img)
 
-def visualize_attention(img, attention, max_size=128., thresh=0.5):
-    img = np.asarray(img)
-    W, H = img.shape[1], img.shape[0]
-    ratio = max_size / max(W, H)
-    W, H = int(W * ratio), int(H * ratio)
-    img = imresize(img, (H, W))
-    attention, preds = attention
-    fig, plots = plt.subplots(len(preds), 1, figsize=(100, 40))
-    for i, p in enumerate(preds):
-        resized_attention = imresize(attention[i].data.cpu().numpy(), (H, W))
-        resized_attention = resized_attention / resized_attention.max()
-        mask = resized_attention > thresh
-        masked_img = img * mask[:, :, None]
-        plots[i].set_title(p)
-        plots[i].imshow(masked_img)
-    plt.tight_layout()
-    plt.show()
+def sample(dset, caption_model, index=None):
+    sample_index = np.random.choice(len(dset)) if index is None else index
+    sample_img_filename = dset.file_names[sample_index] + ".png"
+    sample_img, sample_target = dset.get_sample(sample_index)
+    predicted_target = caption_model.describe(sample_img)
+    return (sample_img, sample_img_filename), sample_target, predicted_target.decode('utf-8').split(' ')
+
+def return_list_with_tabs(li):
+    elems = map(lambda x: x.replace("\\n","\n").replace("\\t", "\t"), li)
+    return ' '.join(elems)
+
+def get_bleu_score(dset, caption_model, num=1):
+    refs = []
+    hyps = []
+    bar = progressbar.ProgressBar()
+    for i in bar(range(num)):
+        _, ref, hyp = sample(dset, caption_model, i)
+        refs.append(ref)
+        hyps.append(hyp)
+    return get_bleu(hyps, refs)
+
+def print_a_sample(dset, caption_model):
+    (img, img_fname), target, predicted = sample(dset, caption_model)
+    print("Sample Image Path: \n", img_fname)
+    print("Sample Target: \n", return_list_with_tabs(target))
+    print("Sample Predicted: \n", return_list_with_tabs(predicted))
 
 if __name__ == '__main__':
     checkpoint = load_url(
-        'model_best.pth.tar', model_dir="results/pix2code_devsupport_resnet50_finetune/", map_location={'gpu:0': 'cpu'})
+        'model_best.pth.tar', model_dir="results/pix2code_devsupport_densenet121_finetune/", map_location={'gpu:0': 'cpu'})
     model = Img2Seq(**checkpoint['config'].model_config)
     model.load_state_dict(checkpoint['state_dict'])
     img_transform, target_tok = checkpoint['tokenizers'].values()
-
+    beam_size = 3
     caption_model = CaptionGenerator(model,
                                      img_transform=img_transform,
                                      target_tok=target_tok,
-                                     beam_size=5,
-                                     get_attention=True,
-                                     max_sequence_length=100,
-                                     length_normalization_factor=10.0,
+                                     beam_size=beam_size,
+                                     get_attention=False,
+                                     max_sequence_length=300,
+                                     length_normalization_factor=2.0,
                                      cuda=True,
-                                     length_normalization_const=5)
+                                     length_normalization_const=2)
 
-    image_filename = "/home/vigi99/devsupportai_ui_gen/eval_set_pix2code/89E58377-5F3C-440D-A97E-08C05A3D039B.png"
-    img, (caption, attentions) = show_and_tell(image_filename)
-    print(caption)
+    dset = Pix2CodeDataset("/home/vigi99/devsupportai_ui_gen/eval_set/")
+    # print_a_sample(dset, caption_model)
+    num_of_examples = len(dset)
+    bleu = get_bleu_score(dset, caption_model, num_of_examples)
+    print('Bleu Score for {0:d} examples with beam size {2:d} is {1:.2f}'.format(num_of_examples, bleu, beam_size))
--- scripts/train/train_pix2code.sh
+++ scripts/train/train_pix2code.sh
@@ -3,25 +3,25 @@ DATASET_DIR=${1:-"/home/vigi99/devsupportai_ui_gen/"}
 OUTPUT_DIR=${2:-"./results"}
 
 python main.py \
-  --save pix2code_devsupport_resnet50_finetune \
-  --resume ${OUTPUT_DIR}/pix2code_devsupport_resnet50_finetune/model_best.pth.tar \
+  --save pix2code_devsupport_densenet121_finetune \
+  --resume ${OUTPUT_DIR}/pix2code_devsupport_densenet121_finetune/model_best.pth.tar \
   --dataset ${DATASET} \
   --dataset_dir ${DATASET_DIR} \
   --results_dir ${OUTPUT_DIR} \
   --model Img2Seq \
   --model_config "{'encoder': {'model': 'densenet121', 'finetune': True}, \
-                   'decoder': {'num_layers': 3, 'hidden_size': 64, 'dropout': 0.2, \
-                               'tie_embedding': False, 'mode': 'GRU',\
+                   'decoder': {'num_layers': 3, 'hidden_size': 128, 'dropout': 0.2, \
+                               'tie_embedding': True, 'mode': 'GRU',\
                                'attention': {'mode': 'bahdanau', 'normalize': True}}}" \
   --data_config "{'image_size': (256,256)}" \
-  --b 16 \
+  --b 32 \
   --start-epoch 0 \
-  --epochs 50 \
-  --print-freq 50 \
-  --save-freq 1000 \
-  --eval-freq 500 \
-  --workers 4 \
+  --epochs 200 \
+  --print-freq 40 \
+  --save-freq 200 \
+  --eval-freq 200 \
+  --workers 3 \
   --trainer Img2SeqTrainer \
   --optimization_config "[{'epoch': 0, 'optimizer': 'Adam', 'lr': 1e-3},
-                          {'epoch': 2, 'optimizer': 'Adam', 'lr': 1e-4},
-                          {'epoch': 8, 'optimizer': 'SGD', 'lr': 1e-4, 'momentum': 0.9}]"
+                          {'epoch': 10, 'optimizer': 'Adam', 'lr': 1e-4},
+                          {'epoch': 60, 'optimizer': 'SGD', 'lr': 1e-4, 'momentum': 0.9}]"
